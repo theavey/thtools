dc2abef4de53e3ab53e704300789e9265ad1118d
Thomas Heavey <thomasjheavey@gmail.com>
2017-10-10 11:16:45 -0400

    Added tools for using pickle

---
+++ thtools/saving.py
@@ -0,0 +1,38 @@
+"""
+Functions for saving fairly arbitrary python objects
+
+"""
+########################################################################
+#                                                                      #
+# This script was written by Thomas Heavey in 2017.                    #
+#        theavey@bu.edu     thomasjheavey@gmail.com                    #
+#                                                                      #
+# Copyright 2017 Thomas J. Heavey IV                                   #
+#                                                                      #
+# Licensed under the Apache License, Version 2.0 (the "License");      #
+# you may not use this file except in compliance with the License.     #
+# You may obtain a copy of the License at                              #
+#                                                                      #
+#    http://www.apache.org/licenses/LICENSE-2.0                        #
+#                                                                      #
+# Unless required by applicable law or agreed to in writing, software  #
+# distributed under the License is distributed on an "AS IS" BASIS,    #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      #
+# implied.                                                             #
+# See the License for the specific language governing permissions and  #
+# limitations under the License.                                       #
+#                                                                      #
+########################################################################
+
+
+import pickle
+
+
+def save_obj(obj, name):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
