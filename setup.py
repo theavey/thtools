56c3092f0462d05e315c04e083614a3c2aea4631
Thomas Heavey <thomasjheavey@gmail.com>
2018-08-07 11:46:01 -0400

    added setuptools setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='thtools',
+    packages=['thtools'],
+    url='https://github.com/theavey/thtools',
+    author='Thomas Heavey',
+    author_email='thomasjheavey@gmail.com',
+    description='Tools and such I use regularly for productivity',
+    install_requires=[],
+    zip_safe=True,
+)
+
